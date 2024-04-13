316eee6ac980f06c7cd1a9e99e03755ddb2921d5
ringlu <ringlu@tencent.com>
2024-04-13 22:48:14 +0800

    feat: log输出到less可以上下滑动

--- tig_log.py
+++ tig_log.py
@@ -1,18 +1,26 @@
+import subprocess
 from tig_common import *
 
 def tig_log():
     commit_info = read_commit_info()
+    content = ""
     while(commit_info):
-        print_commit_info(commit_info)
+        content += content_from_commit_info(commit_info)
         parents = commit_info.get("parent")
         if not parents:
-            return
+            break
         # TODO: 暂时只取第一个
         last_commit_md5 = parents[0]["md5"]
         commit_info = read_commit_or_tree_info(last_commit_md5)
+    
+    print(content)
+    less_process = subprocess.Popen(['less'], stdin=subprocess.PIPE)
+    less_process.communicate(input=content.encode())
 
-def print_commit_info(commit_info):
-    print("commit ", commit_info["md5"])
-    print("Date:\t", commit_info["time"])
-    print("\t", commit_info["message"])
-    print("\n")
+def content_from_commit_info(commit_info):
+    content = ""
+    content += "\ncommit " + commit_info["md5"]
+    content += "\nDate:\t" + commit_info["time"]
+    content += "\n\t" + commit_info["message"]
+    content += "\n"
+    return content
